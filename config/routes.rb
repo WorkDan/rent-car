15d30dd6eab42b7c1d308d63e13eff14e27b42ad
Daniel <saifulindaniel@gmail.com>
2019-04-04 00:06:33 +0600

    Added log in page, with some fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users,
     path: '',
-    path_names:  { sign_up: :sign_up, sign_out: :logout },
-    controllers: { registrations: :registrations }
+    path_names:  { sign_up: :sign_up, sign_out: :logout, sign_in: :login },
+    controllers: { registrations: :registrations, sessions: :sessions }
 
     root 'home#index'
 end
